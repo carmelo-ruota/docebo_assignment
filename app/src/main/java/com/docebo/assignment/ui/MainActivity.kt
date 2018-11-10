b5e3f25fed7a0dccad11e0e6bf7d62697227cd33
Carmelo Ruota <carmelo.ruota@skylabs.it>
2018-11-10 23:14:15 +0100

    Add navigation

--- app/src/main/java/com/docebo/assignment/ui/MainActivity.kt
+++ app/src/main/java/com/docebo/assignment/ui/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.docebo.assignment
+package com.docebo.assignment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.docebo.assignment.R.layout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
     }
 }
