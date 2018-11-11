b3be80ebbc2aaa7d84cf32e91f7990f91a0be90d
Carmelo Ruota <carmelo.ruota@skylabs.it>
2018-11-11 18:12:10 +0100

    Add search screen UI

--- app/src/main/java/com/docebo/assignment/ui/search/SearchFragment.kt
+++ app/src/main/java/com/docebo/assignment/ui/search/SearchFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.docebo.assignment.R
 import com.docebo.assignment.R.layout
 import com.docebo.assignment.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_search.button_search
+import kotlinx.android.synthetic.main.fragment_search.spinner_type
+import java.util.Collections
 
 
 class SearchFragment : BaseFragment() {
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -16,4 +22,21 @@ class SearchFragment : BaseFragment() {
         return inflater.inflate(layout.fragment_search, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setTypeAdapter()
+        button_search.setOnClickListener {
+            //TODO launch search
+        }
+    }
+
+    private fun setTypeAdapter() {
+        val adapter = ArrayAdapter.createFromResource(
+                activity,
+                R.array.type_array,
+                android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner_type.adapter = adapter
+    }
 }
